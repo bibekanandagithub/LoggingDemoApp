96be8b0a1a56ace18853d9862ad867c07cf709d9
bibekananda.panigrahi@outlook.com <bibekananda.panigrahi@outlook.com>
2019-11-06 07:11:26 +0530

    Add project files.

---
+++ Tuple/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ValueTuple<int, string> obj = new ValueTuple<int, string>(1, "Bibekananda");
+            Console.WriteLine(obj.Item1+"  "+obj.Item2);
+            Console.Read();
+        }
+
+        
+    }
+}
